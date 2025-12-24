778f2df77a3d9b4caf649b599c405be6d6ce55e6
Om Panpatil <ompanpatil513@gmail.com>
2025-12-24 17:06:35 +0530

    Based

---
+++ Frontend/Basic/D4/script.js
@@ -0,0 +1,8 @@
+console.log("Hello world..");
+console.warn("This is a warning");
+console.error("This is an error");
+var abs = confirm('Are u sure u want to do all of these ?');
+console.log(abs);
+let x = 'LinkinPark';
+let y = 'Numb';
+console.log(x + ' ' + y);
