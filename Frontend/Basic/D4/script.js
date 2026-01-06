334eb05e65ac849f4d4a6b3d96e2e8e403399a72
Om Panpatil <ompanpatil513@gmail.com>
2026-01-06 17:08:14 +0530

    Based

--- Frontend/Basic/D4/script.js
+++ Frontend/Basic/D4/script.js
@@ -144,4 +144,51 @@ let cali = document.querySelector("#cali");
 cali.addEventListener("DO push urself!", function(){
     alert("Let's go 😀");
 });
-document.querySelector("cali").dispatchEvent(calievent);
+// document.querySelector("cali").dispatchEvent(calievent);
+
+
+// let iamge1 = document.querySelector('#abcd');
+// let iamge2 = document.querySelector('.abcd');
+
+// let firsttext = document.querySelector('.def');
+// let secondtext = document.querySelector('#def');
+
+// Agar aapke pass bohot saare h3 hai and aap select karte ho toh sirf pehla walla h3 select hoga.
+
+
+let heads2 = document.querySelector("h2");
+heads2.textContent += "Om";
+heads2.innerHTML += '<i> Helo </i>';
+
+// function namekar(){
+
+// }
+
+let calbtn = document.querySelector('#cali');
+heads2.style.color = 'red';
+heads2.style.fontFamily = 'Gilroy';
+heads2.style.fontWeight = '300';
+heads2.classList.add('makeitfontss');
+
+let headddd = document.createElement('h1');
+headddd.textContent = "Haahahaahaa hehe";
+
+// document.querySelector("body").appendChild(headddd);
+// let headddd2 = document.querySelector('headddd2');
+// headddd2.style.color = 'blue';
+
+let imaggg = document.createElement('img');
+imaggg.src='https://images.unsplash.com/photo-1767503263183-39c0b7ff96a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+document.querySelector("body").appendChild(imaggg);
+imaggg.style.padding = '4em';
+imaggg.classList.add('makeitfine');
+
+
+calbtn.addEventListener('click', function(){
+    calbtn.style.backgroundColor = 'orange';
+    calbtn.style.color = 'white';
+    calbtn.style.fontWeight = '500';
+    calbtn.style.fontSize = '1vw';
+    alert("Let's do it!");
+    // screen.style.backgroundColor = 'black';
+})
