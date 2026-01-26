2972d749f9d9360c7a3bea13c420a3b9302e3b5b
Om Panpatil <ompanpatil513@gmail.com>
2026-01-26 14:21:16 +0530

    Based

---
+++ Frontend/Basic/DLearn/script.js
@@ -0,0 +1,40 @@
+// let boxss = document.querySelector('#box');
+// boxss.style.color = 'white';
+
+let boxss = document.createElement('div');
+boxss.style.height = '200px';
+boxss.style.width = '300px';
+boxss.style.backgroundColor = 'blue';
+document.body.appendChild(boxss);
+boxss.style.position = 'absolute';
+boxss.style.top = '50%';
+boxss.style.left = '50%';
+boxss.style.transform = 'translate(-50% , -50%)';
+
+let btns = document.createElement('button');
+btns.innerHTML = 'CLick Me';
+document.body.appendChild(btns);
+btns.style.position = 'absolute';
+btns.style.top = '70%';
+btns.style.left = '50%';
+btns.style.transform = 'translate(-50% , -50%)';
+btns.style.padding = '10px 20px';
+
+btns.addEventListener('click', function(){
+    boxss.style.backgroundColor = 'red';
+})
+
+let h5 = document.createElement('h5');
+h5.innerHTML = 'Stranger';
+document.body.appendChild(h5);
+h5.style.fontSize = '30px';
+h5.style.textAlign = 'center';
+h5.style.marginTop = '20px';
+
+let btnsss = document.createElement('button');
+btnsss.innerHTML = 'Friendhip';
+document.body.appendChild(btnsss)
+btnsss.style.backgroundColor = 'yellow';
+btnsss.style.padding = '10px 20px';
+
+
